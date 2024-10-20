b454f9a11c1ee90554a01fcdfca2dbb610a24e30
rasunM <rasunmanohara@gmail.com>
2024-10-20 19:52:38 +0530

    error fixs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'dist',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig;
