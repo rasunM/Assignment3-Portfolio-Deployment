c3bda5abe37aff6777f9c627e7cf161db8940cc6
rasunM <rasunmanohara@gmail.com>
2024-10-20 18:34:41 +0530

    solve run time error

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import Footer from '../components/Footer';
 import Banner from '../components/HomeComponents/Banner';
 import MyExpertise from '../components/HomeComponents/Expertise/MyExpertise';
 
+export const runtime = "edge";
+
 const home = () => {
     return (
         <div className="Home-Page -z-10">
